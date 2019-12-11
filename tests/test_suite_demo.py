f82df8ae5e9a667f1245dadc4aedf76bc8cfd37f
kiril <kirilk@affilomania.com>
2019-12-11 17:04:13 +0200

    .

--- tests/test_suite_demo.py
+++ tests/test_suite_demo.py
@@ -1,24 +1,21 @@
-
 import unittest
 # import your test modules
 from tests.home.login_tests import LoginTests
 from tests.search.search_test import SearchTest
 from tests.search.search_test_csv import SearchTest1Two
 
-
-
 # initialize the test suite
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
-
 # add tests to the test suite
 suite.addTests(loader.loadTestsFromModule(LoginTests))
-suite.addTests(loader.loadTestsFromModule(LoginTests))
 suite.addTests(loader.loadTestsFromModule(SearchTest1Two))
-
-
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
 result = runner.run(suite)
+
+# run the suit >>> pytest -n 2 tests\test_suite_demo.py --browser firefox
+# run  parallelization
+# pip install pytest-xdist  >> pytest -n NUM
